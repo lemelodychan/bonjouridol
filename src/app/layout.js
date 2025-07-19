db9c80b2832a1249a14177b8abe49154c908be95
Melody <elodie.dassance@gmail.com>
2025-07-19 23:01:01 +0900

    GTM attempt

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import { PrismicPreview, PrismicNextImage } from '@prismicio/next'
 import { repositoryName, createClient } from '@/prismicio'
 import Script from 'next/script';
 import { Analytics } from "@vercel/analytics/next"
-import GoogleAnalytics from './components/GoogleTagManager'
 
 import Navbar from './components/Navbar'
 import Topbar from './components/Topbar'
@@ -52,12 +51,11 @@ export default async function RootLayout({ children }) {
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="google-site-verification" content="WOV1O-V5Z53289sOWAWW_caWIAFnluDcQ6PEdiJ0pCU" />
-          <GoogleAnalytics />
-          <GoogleTagManager />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,700;1,9..40,400;1,9..40,500;1,9..40,700&family=Noto+Sans+JP:wght@400;500;700&family=Poppins:wght@900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet"></link>
+          <GoogleTagManager />
         </head>
         <body>
           <ErrorHandler />
@@ -74,9 +72,10 @@ export default async function RootLayout({ children }) {
       console.error("Error in layout:", error);
       return (
         <html lang="en">
-          <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          </head>
+                  <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <GoogleTagManager />
+        </head>
           <body>
             <ErrorHandler />
             <Topbar />
