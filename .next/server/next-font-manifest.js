99a7c3c3f9aa2f6d3a0ea6b6de6a214012dd8b1e
Melody <elodie.dassance@gmail.com>
2025-07-22 13:28:59 +0900

    CSP analytics fix

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/elodie/Documents/GitHub/bonjouridol/src/app/layout\":[\"static/media/9c18eba18d4baf22-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
