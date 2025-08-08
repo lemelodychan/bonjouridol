64df03273e5797c3e325747029ba0eeecd38fce0
Melody <elodie.dassance@gmail.com>
2025-08-09 08:25:51 +0900

    Umami tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -57,6 +57,7 @@ export default async function RootLayout({ children }) {
           <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,700;1,9..40,400;1,9..40,500;1,9..40,700&family=Noto+Sans+JP:wght@400;500;700&family=Poppins:wght@900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet"></link>
           <GoogleTagManager />
+          <script defer src="https://cloud.umami.is/script.js" data-website-id="f092e573-6aba-45f6-af52-71e7d3c51bd0"></script>
         </head>
         <body>
           <ErrorHandler />
