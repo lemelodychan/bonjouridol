3464b779fbe0310fbdc98617f79d9f539c3fef73
Melody <elodie.dassance@gmail.com>
2025-12-29 16:15:29 +0900

    Page builder edits

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -5,6 +5,7 @@ import type * as prismic from "@prismicio/client";
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
 type ArticlesDocumentDataSlicesSlice =
+  | TableContentsSlice
   | SeparatorSlice
   | InterviewSlice
   | PurchaseSlice
@@ -792,6 +793,7 @@ export type HomepageDocument<Lang extends string = string> =
   >;
 
 type PageDocumentDataSlicesSlice =
+  | TableContentsSlice
   | InterviewSlice
   | SeparatorSlice
   | ContactFormSlice
